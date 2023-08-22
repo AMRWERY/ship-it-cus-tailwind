1abae8a7795543180adbed8166f26a63d57e3fc9
amr <amrmounir2@gmail.com>
2023-08-23 00:42:44 +0300

    add navigation in TodayDeals component

--- src/components/TodayDeals.vue
+++ src/components/TodayDeals.vue
@@ -78,9 +78,11 @@
                             </div>
                         </div>
                         <div class="flex">
-                            <button
-                                class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Shop
-                                Now</button>
+                            <router-link to="/products">
+                                <button
+                                    class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Shop
+                                    Now</button>
+                            </router-link>
                         </div>
                     </div>
                 </div>
